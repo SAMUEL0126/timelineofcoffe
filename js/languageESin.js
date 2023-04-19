799464a076c9d67e8d9b5fb928df849d8f453446
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-04-19 14:56:01 -0500

    transision para el page de ingles creada

--- js/languageESin.js
+++ js/languageESin.js
@@ -14,7 +14,7 @@ let contLoading = document.getElementById('contLoadingHidden')
 let lentDarkT = document.getElementById('lentDarkT')
 
 español.addEventListener('click', functionactiva)
-
+ingles.addEventListener('click', functionactivaIn)
 function functionactiva() {
     contLoading.classList.toggle('contLoadingHidden')
     contLoading.classList.toggle('contLoadingVisisble')
@@ -42,3 +42,30 @@ function functionactiva() {
     }, 20)
 }
 
+// enviar a page en ingles 
+function functionactivaIn() {
+    contLoading.classList.toggle('contLoadingHidden')
+    contLoading.classList.toggle('contLoadingVisisble')
+
+    contpageClock.classList.toggle('contpageClock')
+    contpageClock.classList.toggle('contpageClockHidden')
+
+
+    // get reference to our Elemets
+    let number = document.getElementById('number')
+    let counter = 0
+
+    setInterval(() => {
+        if (counter == 100) {
+            clearInterval
+            lentDarkT.classList.add('lentDarkTransition')
+            setTimeout(() => {
+                window.location.assign('/pages/presentacionIng.html')
+            }, 3000);
+        } else {
+            counter += 1
+            number.innerHTML = `${counter}%`
+        }
+
+    }, 20)
+}
