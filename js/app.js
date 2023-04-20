7c5bd66779ec52bff9d5a24df74f2803e3699d23
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-04-20 09:59:25 -0500

    funcionalidad del reloj casi terminada

--- js/app.js
+++ js/app.js
@@ -3,10 +3,7 @@ let next = 1928
 
 let btnN = document.getElementById('next')
 let btnB = document.getElementById('prev')
-// window.addEventListener('load', () => {
-//     // btnB.textContent = back
-//     // btnN.textContent = next
-// })
+
 document.getElementById('next').onclick = function () {
     
     let lists = document.querySelectorAll('.item')
@@ -42,51 +39,76 @@ document.getElementById('prev').onclick = function () {
 
 /*--------  Temporizador -----------*/
 
-let workTime = 10
-let breakTime = 5
 
-let seconds = "00"
 
-function start() {
+let minutos = 1
+let seconds = 0
 
-    seconds = 59
+cargarSeconds()
 
-    let workMinutes = workTime - 1
-    let breakMinutes = breakTime - 1
+//definimos y ejecutamos los segundos
 
-    breakCount = 0
-
-    let timerFunction = () => {
-        document.getElementById('minutes').innerHTML = workMinutes
-        document.getElementById('seconds').innerHTML = seconds
+function cargarSeconds() {
+    let txtSecond
     
-        seconds = seconds - 1 
 
-        if( seconds == 0){
-            workMinutes = workMinutes - 1
-            if(workMinutes == -1){
-                if(breakCount % 2 == 0){
-                    workMinutes = breakMinutes
-                    breakCount++ 
-
-
-                }else{
-                    workMinutes = workTime
-                    breakCount++
-
-                   
-                }
-            }
-            seconds = 59
-        }
+    if (seconds < 0) {
+        seconds = 59
     }
 
-    setInterval(timerFunction , 1000)
+    //agregar 0 a los segundos despues de bajar de dos cifras
+    if (seconds < 10) {
+        txtSecond = `0${seconds}`
+    }
+    else {
+        txtSecond = seconds
+    }
+    //enviamos los valores a plazmarse
+    document.getElementById('seconds').textContent = txtSecond
+    seconds--
+    cargarMinutos(seconds)
+}
+
+function cargarMinutos(seconds) {
+    let textMinutos
+
+    if (seconds == -1 && minutos !== 0) {
+        setTimeout(() => {
+            minutos--
+        }, 500);
+    }
+    else if (seconds == -1 && minutos == 0) {
+        setTimeout(() => {
+            minutos = 00;
+        }, 500)
+    }
+    else if (seconds == 0 && minutos == 0) {
+        setTimeout(() => {
+            minutos = 00;
+            seconds = 00
+        }, 500)
+    }
+
+    //Mostrar minutos en pantalla
+    if (minutos < 10) {
+        textMinutos = `0${minutos}`;
+    } else {
+        textMinutos = minutos;
+    }
+
+    document.getElementById('minutes').textContent = textMinutos
+    render(minutos, seconds)
 
 }
 
-window.addEventListener('load', () => {
-    document.getElementById('minutes').innerHTML = workTime
-    document.getElementById('seconds').innerHTML = seconds
-    start()
-})
+function render(minutos, seconds) {
+    if (minutos == 0 && seconds == 0) {
+        // setTimeout(() => {
+        //     $('#darWindowLineT').addClass('darWindowLineRen')
+        // }, 900);
+        setTimeout(() => {
+            window.location.assign('/pages/languageESin.html')
+        }, 2000);
+    }
+}
+setInterval(cargarSeconds, 1000);
