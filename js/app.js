b4080cdbfc09f03cdb7dc96fd9e620f709c95be4
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-04-21 22:54:29 -0500

    año 1965 implementado

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ document.getElementById('prev').onclick = function () {
         back = 1927
 
         next = 1928
-        slide.appendChild(lists[19])
+        slide.appendChild(lists[20])
     }
 }
 
