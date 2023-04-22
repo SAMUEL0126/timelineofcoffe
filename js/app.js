d9684da88db3e84bdf3d905f49dd7d62c43226a5
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-04-22 14:49:27 -0500

    año 1980 establecido

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ document.getElementById('prev').onclick = function () {
         back = 1927
 
         next = 1928
-        slide.appendChild(lists[32])
+        slide.appendChild(lists[34])
     }
 }
 
