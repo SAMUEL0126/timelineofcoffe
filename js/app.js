ecce7bd1ee1d196f716a47982b64ed3da6f8f8e7
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-04-23 07:03:02 -0500

    ajuste en app

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ document.getElementById('prev').onclick = function () {
         back = 1927
 
         next = 1928
-        slide.appendChild(lists[62])
+        slide.appendChild(lists[63])
     }
 }
 
