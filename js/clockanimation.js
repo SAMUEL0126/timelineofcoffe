ced7f2c22729b6c4c7405d9114298e9a78739cce
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-05-15 20:15:50 -0500

    reloj temporalmente finalizado

---
+++ js/clockanimation.js
@@ -0,0 +1,19 @@
+const body = document.querySelector("body"),
+hourhand = document.querySelector(".hour"),
+minutehand = document.querySelector(".minute"),
+secondhand = document.querySelector(".second")
+
+const updatetime = () =>{
+console.log();
+let date = new Date(),
+secToDeg = (date.getSeconds()/60) * 360;
+minToDeg = (date.getMinutes()/60) * 360;
+hrToDeg = (date.getHours()/12) * 360;
+
+secondhand.style.transform = `rotate(${secToDeg}deg)`
+minutehand.style.transform = `rotate(${minToDeg}deg)`
+hourhand.style.transform = `rotate(${hrToDeg}deg)`
+
+}
+setInterval(updatetime,1000)
+updatetime()
